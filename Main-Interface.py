15c76b5e7074c5fcae0d69b0f62537a8e6c0646b
JMeyerA <jmeyer2843@conestogac.on.ca>
2024-04-05 13:59:59 -0400

    Template

--- Main-Interface.py
+++ Main-Interface.py
@@ -1,1 +1,16 @@
-PermissionError
+# Final Python Project Word editor
+# Jack Meyer
+# Date: 2024-04-05
+# File Editor  
+
+#sign in page
+
+#end of Sign in page
+
+#file selector
+
+# end of file selector
+
+# Text editor
+
+# end of text editor
