93a8a5707a1bb98923c00ae1f03baae38672be69
JMeyerA <jmeyer2843@conestogac.on.ca>
2024-04-11 22:10:01 -0400

    Removed unused code

--- Main-Interface.py
+++ Main-Interface.py
@@ -6,9 +6,9 @@
 import os #imports os library
 import platform #imports platform to check the OS platform for Combatibilty
 osCheck = platform.system().lower()
-if osCheck != "windows":
-    while True:
-        print("You are not running a Windows")
+if osCheck != "windows": #if os is not windows
+    while True: #run loop to prevent crash on non windows system
+        print("You are not running a Winodws OS")
         input("Please Run this code on a Windows machine")
 
 validAnswers = {} # dictonary of acepted answers 
@@ -16,29 +16,6 @@ validAnswers["no"] = ["no","n"] #adding a list to the dictonary
 validAnswers["yes"] = ["yes","y","ye"] #adding a list to the dictonary
 validAnswers["all"] = ["no","n","yes","y","ye"]
 
-def userConfermation(info): #confirms that the data they entered is correct feed in users inputed data to function place inside of user input loop
-    while validate not in validAnswers["all"]: # begining of loop Info Validate 
-        print(info)  
-        validate = input("Is the information correct? Yes/No ").strip().lower() #asks user if inputs are correct
-        
-        if validate in validAnswers["yes"]:  #checks if user input is in list of yesAnswers then breaks Info validate loop
-            print ("Input Accepted\n ")
-            break
-        elif validate in validAnswers["no"]:      #if user input is in list of noAnswers breaks info validate loop and send user to begining of Info Input loop
-            print ("Please enter info again\n ")
-            break
-        else:       # if not a Y or N raises value error
-            print("Value Error Enter a yes or no\n ")
-            continue
-#code to be used with userconfermation function
-''' code below to be entered outside of the function
-    # if validateInfo() in yesAnswers: #if yes was answered continues Order loop
-    #    break
-    #else:                   #if no was answered breaks order loop
-    #   continue
-    #
-     #end of Info validate loop
-'''
      
 ''' end of setup code'''
 
@@ -158,13 +135,22 @@ main()
 #Text editor functions
 selectedFile = (r"user-Docs\test.txt") #used for testing before file selector is done
 def readFile(): #reads the file out to the user
+    
+    #selected file goes into function
+    #opens the file
+    #prints contents of file to user for viewing4
+    
     file = open(f"{selectedFile}","r") #opens the file
     print(f"\n{file.read()}") #prints out the file into the cmd for the user to read
     file.close()
 def editFile():
+    #takes selected file 
+    #opens file in notepad
     print("Opening Notepad")
     os.system(f"notepad {selectedFile}") #opens the selected file in notepad.exe
 def openFile(): #used to ask user if they want to edit or read the file
+    #asks the user what they would like to do with the selected file
+    #returns the users choice to the main code
     while True:
         try:
             validResponses = ["1","2","read","edit"]
@@ -185,19 +171,22 @@ def openFile(): #used to ask user if they want to edit or read the file
         editFile()
     return userediting
 def restartCode():
+    #asks the user if they would like to restart their code
+    #checks if user input is a valid answer and if it is breaks loop
+    #if users says yes then code returns true restart value
     restart = ""
-    while restart not in validAnswers["all"]:
+    while restart not in validAnswers["all"]: #will loop until a valid answer is inputed 
         print("Would You Like to Select a Diffrent File")
         print("\nPlease Enter Valid Response of Yes/No\n ")
         restart = input(": ").strip().lower()
-        return restart
+    return restart #returns value to main code
 '''end of text editor functions'''
    
 #code starts here     
 while True:
-    print(f"You have selected {selectedFile}")
+    print(f"You have selected {selectedFile}") #tells user what file is selected
     userediting = openFile() #runs the edit function
-    if userediting == True:
+    if userediting == True: #if the user selected editing 
         input("Please Hit enter once the file is done being edited") #if the user is in notepad waits for user to hit enter before continuing
     restart = restartCode() #runs restart code function to see if the user wants to pick a new file
         
